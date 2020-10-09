c254359984036d8221a5487185e95b89be155e74
dylan@fampiera.nl <dylan@fampiera.nl>
2020-10-09 13:47:25 +0200

    Hotfix: Max Evasion from Stats

--- module/actor/calculations/evasion-calculator.js
+++ module/actor/calculations/evasion-calculator.js
@@ -1,13 +1,13 @@
 export function CalculateEvasions(data) {
     let evasion = {
-        "physical": Math.floor(data.stats.def.total / 5),
-        "special": Math.floor(data.stats.spdef.total / 5),
-        "speed": Math.floor(data.stats.spd.total / 5)
+        "physical": Math.min(Math.floor(data.stats.def.total / 5),6),
+        "special": Math.min(Math.floor(data.stats.spdef.total / 5),6),
+        "speed": Math.min(Math.floor(data.stats.spd.total / 5),6)
     };
 
-    let globalMod = 0;
+    let globalMod = parseInt(data.modifiers.evasion) || 0;
 
-    if (data.modifiers.hardened) globalMod++;
+    if (data.modifiers.hardened && data.health.injuries >= 3) globalMod++;
 
     if(globalMod != 0) evasion = Object.fromEntries(Object.entries(evasion).map(([key, value]) => [key, value + globalMod]));
 
