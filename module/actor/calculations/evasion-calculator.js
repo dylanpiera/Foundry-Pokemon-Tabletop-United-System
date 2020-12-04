2c03143cff5e10dd35baca498a2c83f23a88261c
dylan@fampiera.nl <dylan@fampiera.nl>
2020-12-04 16:51:59 +0100

    Add Evasion Modifier Fields
    Resolves #37

--- module/actor/calculations/evasion-calculator.js
+++ module/actor/calculations/evasion-calculator.js
@@ -1,11 +1,11 @@
 export function CalculateEvasions(data) {
     let evasion = {
-        "physical": Math.min(Math.floor(data.stats.def.total / 5),6),
-        "special": Math.min(Math.floor(data.stats.spdef.total / 5),6),
-        "speed": Math.min(Math.floor(data.stats.spd.total / 5),6)
+        "physical": Math.max(Math.min(Math.floor(data.stats.def.total / 5),6) + data.modifiers.evasion.physical, 0),
+        "special": Math.max(Math.min(Math.floor(data.stats.spdef.total / 5),6) + data.modifiers.evasion.special, 0),
+        "speed": Math.max(Math.min(Math.floor(data.stats.spd.total / 5),6) + data.modifiers.evasion.speed, 0)
     };
 
-    let globalMod = parseInt(data.modifiers.evasion) || 0;
+    let globalMod = 0;
 
     if (data.modifiers.hardened && data.health.injuries >= 3) globalMod++;
 
