cc1cfc7b37bf94d921d644f1f81a163018fd31c5
GSV A Nest of Kindled Embers <69528253+GSV-a-Nest-of-Kindled-Embers@users.noreply.github.com>
2021-03-04 16:24:59 -0500

    Added dex-drag-and-drop-to-map functionality and fixed a token-evolution-name-not-changed bug.
    Added dex-drag-and-drop-to-map functionality and fixed a token-evolution-name-not-changed bug. Also added some system config options for setting directories for pokemon imgpath default and pokemon cry sounds and move sounds. Added a (default off) config option to play a Pokemon's cry when it's dragged onto the field from the dex.

--- module/ptu.js
+++ module/ptu.js
@@ -17,6 +17,7 @@ import { insurgenceData, sageData, uraniumData } from "./data/fangame-species-da
 import { DbData } from "./data/db-data.js"
 import { TypeEffectiveness } from "./data/effectiveness-data.js"
 import { PTUPokemonCharactermancer } from './forms/charactermancer-pokemon-form.js'
+import { PTUDexDragOptions } from './forms/dex-drag-options-form.js'
 import { PTUCustomSpeciesEditor } from './forms/custom-species-editor-form.js'
 import { PTUCustomMonEditor } from './forms/custom-mon-editor-form.js'
 import { RollWithDb } from './utils/roll-calculator.js'
@@ -25,6 +26,7 @@ import { ChangeLog } from './forms/changelog-form.js'
 import { applyDamageToTargets, undoDamageToTargets }  from './combat/damage-calc-tools.js'
 import CustomSpeciesFolder from './entities/custom-species-folder.js'
 import { CreateMonParser } from './utils/species-command-parser.js'
+import { FinishDexDragPokemonCreation } from './utils/species-command-parser.js'
 import { GetRandomNature } from './utils/random-nature-generator.js'
 import { GiveRandomAbilities } from './utils/random-abilities-generator.js'
 import { GiveLatestMoves } from './utils/latest-moves-generator.js'
@@ -46,6 +48,7 @@ Hooks.once('init', async function() {
     PTUActor,
     PTUItem,
     PTUPokemonCharactermancer,
+    PTUDexDragOptions,
     PTUCustomSpeciesEditor,
     PTUCustomMonEditor,
     levelProgression,
@@ -56,6 +59,8 @@ Hooks.once('init', async function() {
     TypeEffectiveness,
     GetSpeciesData,
     RollWithDb,
+    PlayPokemonCry,
+    FinishDexDragPokemonCreation,
     monGenerator: {
       CreateMonParser,
       GetRandomNature,
@@ -395,6 +400,44 @@ function _loadSystemSettings() {
     default: false,
     onChange: (value) => CustomSpeciesFolder.updateFolderDisplay(value)
   });
+
+  game.settings.register("ptu", "defaultPokemonImageDirectory", {
+    name: "Default Pokemon Image Directory",
+    hint: "This will play a Pokemon's cry when it is drag-and-dropped from the pokedex compendium",
+    scope: "world",
+    config: true,
+    type: DirectoryPicker.Directory,
+    default: "Gen4-Art/",
+  });
+
+  game.settings.register("ptu", "playPokemonCriesOnDrop", {
+    name: "Play Pokemon Cry when dragged from Dex",
+    hint: "This will play a Pokemon's cry when it is drag-and-dropped from the pokedex compendium",
+    scope: "world",
+    config: true,
+    type: Boolean,
+    default: false
+  });
+
+  game.settings.register("ptu", "pokemonCryDirectory", {
+    name: "Pokemon Cry Directory",
+    hint: "The directory where the user can upload mp3 or wav files (named as the lowercase name of the pokemon).",
+    scope: "world",
+    config: true,
+    type: DirectoryPicker.Directory,
+    default: "pokemon_cries/",
+    onChange: (value) => CustomSpeciesFolder.updateFolderDisplay(value)
+  });
+
+  game.settings.register("ptu", "moveSoundDirectory", {
+    name: "Move Sound Directory",
+    hint: "The directory where the user can upload mp3 or wav files (named as the lowercase name of the move) to be used by scripts or modules.",
+    scope: "world",
+    config: true,
+    type: DirectoryPicker.Directory,
+    default: "pokemon_sounds/",
+    onChange: (value) => CustomSpeciesFolder.updateFolderDisplay(value)
+  });
 } 
 
 /* -------------------------------------------- */
@@ -576,3 +619,136 @@ function setAccessabilityFont(enabled) {
     document.querySelector(':root').style.setProperty('--pkmnFontStyle', 'Pokemon GB')
   }
 }
+
+export async function PlayPokemonCry(species) 
+{
+    if(game.settings.get("ptu", "playPokemonCriesOnDrop"))
+    {
+        let CryDirectory = game.settings.get("ptu", "pokemonCryDirectory");
+        let SpeciesCryFilename = species.toString().toLowerCase();
+
+        const response_mp3 = await fetch(CryDirectory+SpeciesCryFilename+".mp3");
+        if (response_mp3.status >= 200 && response_mp3.status <= 299) 
+        {
+            AudioHelper.play({src: CryDirectory+SpeciesCryFilename+".mp3", volume: 0.8, autoplay: true, loop: false}, true);
+        } 
+        else 
+        {
+            const response_wav = await fetch(CryDirectory+SpeciesCryFilename+".wav");
+            if (response_wav.status >= 200 && response_wav.status <= 299) 
+            {
+                AudioHelper.play({src: CryDirectory+SpeciesCryFilename+".wav", volume: 0.8, autoplay: true, loop: false}, true);
+            } 
+        }
+    }
+}
+
+class DirectoryPicker extends FilePicker {
+  constructor(options = {}) {
+    super(options);
+  }
+
+  _onSubmit(event) {
+    event.preventDefault();
+    const path = event.target.target.value;
+    const activeSource = this.activeSource;
+    const bucket = event.target.bucket ? event.target.bucket.value : null;
+    this.field.value = DirectoryPicker.format({
+      activeSource,
+      bucket,
+      path,
+    });
+    this.close();
+  }
+
+  static async uploadToPath(path, file) {
+    const options = DirectoryPicker.parse(path);
+    return FilePicker.upload(options.activeSource, options.current, file, { bucket: options.bucket });
+  }
+
+  // returns the type "Directory" for rendering the SettingsConfig
+  static Directory(val) {
+    return val;
+  }
+
+  // formats the data into a string for saving it as a GameSetting
+  static format(value) {
+    return value.bucket !== null
+      // ? `[${value.activeSource}:${value.bucket}] ${value.path}`
+      // : `[${value.activeSource}] ${value.path}`;
+      ? `${value.path}/`
+      : `${value.path}/`;
+  }
+
+  // parses the string back to something the FilePicker can understand as an option
+  static parse(str) {
+    let matches = str.match(/\[(.+)\]\s*(.+)/);
+    if (matches) {
+      let source = matches[1];
+      const current = matches[2].trim();
+      const [s3, bucket] = source.split(":");
+      if (bucket !== undefined) {
+        return {
+          activeSource: s3,
+          bucket: bucket,
+          current: current,
+        };
+      } else {
+        return {
+          activeSource: s3,
+          bucket: null,
+          current: current,
+        };
+      }
+    }
+    // failsave, try it at least
+    return {
+      activeSource: "data",
+      bucket: null,
+      current: str,
+    };
+  }
+
+  // Adds a FilePicker-Simulator-Button next to the input fields
+  static processHtml(html) {
+    $(html)
+      .find(`input[data-dtype="Directory"]`)
+      .each((index, element) => {
+        // disable the input field raw editing
+        $(element).prop("readonly", true);
+
+        // if there is no button next to this input element yet, we add it
+        if (!$(element).next().length) {
+          let picker = new DirectoryPicker({
+            field: $(element)[0],
+            ...DirectoryPicker.parse($(element).val()),
+          });
+          let pickerButton = $(
+            '<button type="button" class="file-picker" data-type="imagevideo" data-target="img" title="Pick directory"><i class="fas fa-file-import fa-fw"></i></button>'
+          );
+          pickerButton.on("click", () => {
+            picker.render(true);
+          });
+          $(element).parent().append(pickerButton);
+        }
+      });
+  }
+
+  /** @override */
+  activateListeners(html) {
+    super.activateListeners(html);
+
+    // remove unnecessary elements
+    $(html).find("ol.files-list").remove();
+    $(html).find("footer div").remove();
+    $(html).find("footer button").text("Select Directory");
+  }
+}
+
+// this s hooked in, we don't use all the data, so lets stop eslint complaining
+// eslint-disable-next-line no-unused-vars
+Hooks.on("renderSettingsConfig", (app, html, user) => {
+  DirectoryPicker.processHtml(html);
+});
+
+export default DirectoryPicker;
