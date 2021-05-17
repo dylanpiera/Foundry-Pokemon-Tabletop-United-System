08c8b65e43175d6a1681087dbcbccbbdc5a4ff44
Dylan <dylan@fampiera.nl>
2021-05-17 20:19:34 +0200

    Add PTU Settings Menu
    Resolves #94

--- module/ptu.js
+++ module/ptu.js
@@ -44,6 +44,7 @@ import RenderDex from './utils/pokedex.js'
 import TMsData from './data/tm-data.js'
 import PTUActiveEffectConfig from './forms/active-effect-config.js';
 import getTrainingChanges from './data/training-data.js';
+import {PTUSettings, PTUSettingCategories} from './forms/settings.js';
 
 export let debug = (...args) => {if (game.settings.get("ptu", "showDebugInfo") ?? false) console.log("DEBUG: FVTT PTU | ", ...args)};
 export let log = (...args) => console.log("FVTT PTU | ", ...args);
@@ -259,7 +260,9 @@ Hooks.once('init', async function() {
       GetOrCreateCachedItem
     },
     api: new Api(),
-    getTrainingChanges
+    getTrainingChanges,
+    settings: PTUSettings,
+    settingCategories: PTUSettingCategories
   };
 
   /**
@@ -328,6 +331,7 @@ function _loadSystemSettings() {
     config: true,
     type: Boolean,
     default: true,
+    category: "rules"
   });
 
   game.settings.register("ptu", "useTutorPoints", {
@@ -340,7 +344,8 @@ function _loadSystemSettings() {
       "true": "Use Tutor Points",
       "false": "Don't use Tutor Points"
     },
-    default: "true"
+    default: "true",
+    category: "rules"
   });
 
   game.settings.register("ptu", "useDexExp", {
@@ -353,7 +358,8 @@ function _loadSystemSettings() {
       "true": "Use Dex Exp",
       "false": "Don't use Dex Exp"
     },
-    default: "false"
+    default: "false",
+    category: "rules"
   });
 
   game.settings.register("ptu", "nonOwnerCanSeeTabs", {
@@ -362,7 +368,8 @@ function _loadSystemSettings() {
     scope: "world",
     config: true,
     type: Boolean,
-    default: false
+    default: false,
+    category: "general"
   });
 
   game.settings.register("ptu", "dex-permission", {
@@ -378,7 +385,8 @@ function _loadSystemSettings() {
       4: "GM Prompt (**NOT YET IMPLEMENTED**)",
       5: "Always allow Pokédex", 
     },
-    default: 1
+    default: 1,
+    category: "general"
   })
 
   game.settings.register("ptu", "combatRollPreference", {
@@ -393,7 +401,8 @@ function _loadSystemSettings() {
       "always-crit": "Always roll crit damage",
       "both": "Always roll both"
     },
-    default: "situational"
+    default: "situational",
+    category: "combat"
   });
 
   game.settings.register("ptu", "combatDescPreference", {
@@ -408,7 +417,8 @@ function _loadSystemSettings() {
       "snippet-or-full": "Show move snippet, or full effect if unset",
       "show": "Show full effect"
     },
-    default: "snippet"
+    default: "snippet",
+    category: "combat"
   });
 
   game.settings.register("ptu", "defaultPokemonImageDirectory", {
@@ -418,6 +428,7 @@ function _loadSystemSettings() {
     config: true,
     type: DirectoryPicker.Directory,
     default: "Gen4-Art/",
+    category: "general"
   });
 
   game.settings.register("ptu", "leagueBattleInvertTrainerInitiative", {
@@ -426,7 +437,8 @@ function _loadSystemSettings() {
     scope: "world",
     config: true,
     type: Boolean,
-    default: true
+    default: true,
+    category: "combat"
   });
 
   game.settings.register("ptu", "verboseChatInfo", {
@@ -463,7 +475,8 @@ function _loadSystemSettings() {
     scope: "world",
     config: true,
     type: Boolean,
-    default: false
+    default: false,
+    category: "rules"
   });
 
   game.settings.register("ptu", "sageData", {
@@ -472,7 +485,8 @@ function _loadSystemSettings() {
     scope: "world",
     config: true,
     type: Boolean,
-    default: false
+    default: false,
+    category: "rules"
   });
 
   game.settings.register("ptu", "uraniumData", {
@@ -481,7 +495,8 @@ function _loadSystemSettings() {
     scope: "world",
     config: true,
     type: Boolean,
-    default: false
+    default: false,
+    category: "rules"
   });
 
   game.settings.register("ptu", "customSpecies", {
@@ -508,7 +523,8 @@ function _loadSystemSettings() {
     config: true,
     type: Boolean,
     default: false,
-    onChange: (value) => CustomSpeciesFolder.updateFolderDisplay(value)
+    onChange: (value) => CustomSpeciesFolder.updateFolderDisplay(value),
+    category: "other"
   });
 
   game.settings.register("ptu", "playPokemonCriesOnDrop", {
@@ -517,7 +533,8 @@ function _loadSystemSettings() {
     scope: "world",
     config: true,
     type: Boolean,
-    default: false
+    default: false,
+    category: "other"
   });
 
   game.settings.register("ptu", "pokemonCryDirectory", {
@@ -527,7 +544,8 @@ function _loadSystemSettings() {
     config: true,
     type: DirectoryPicker.Directory,
     default: "pokemon_cries/",
-    onChange: (value) => CustomSpeciesFolder.updateFolderDisplay(value)
+    onChange: (value) => CustomSpeciesFolder.updateFolderDisplay(value),
+    category: "other"
   });
 
   game.settings.register("ptu", "moveSoundDirectory", {
@@ -537,7 +555,8 @@ function _loadSystemSettings() {
     config: true,
     type: DirectoryPicker.Directory,
     default: "pokemon_sounds/",
-    onChange: (value) => CustomSpeciesFolder.updateFolderDisplay(value)
+    onChange: (value) => CustomSpeciesFolder.updateFolderDisplay(value),
+    category: "other"
   });
 } 
 
@@ -578,19 +597,18 @@ Hooks.on('dropActorSheetData', function(actor, sheet, itemDropData, ){
   return false;
 });
 
-Hooks.on("renderSettingsConfig", function() {
-  let element = $('#client-settings .tab[data-tab="system"] .module-header')[0];
-  element.outerHTML = `
-  <div class="d-flex flexrow">
-    ${element.outerHTML}
-    <div class="item-controls flexcol" style="align-self: center;flex: 0 0 30%;">
-      <a class="item-control" id="open-custom-species-editor"><i class="fas fa-edit" style="margin-right: 3px;"></i><span class="readable">Edit Custom Species</span></a>
+Hooks.on("renderSettingsConfig", function(esc, html, data) {
+  const element = html.find('.tab[data-tab="system"] .settings-list');
+  let header = element.find(".module-header");
+  element.html(
+    `${header[0].outerHTML}
+    <div>
+      <h3>We have moved!</h3>
+      <p class="notes pb-2">All system settings can now be found in the PTU Settings, right under the section with the Configure Settings button in the sidebar!</p>
+      <button onclick="new game.ptu.settings().render(true);" class="mb-2">Open PTU Settings</button>
     </div>
-  </div>`
-
-  $('#open-custom-species-editor').click(function() { 
-    new game.ptu.PTUCustomSpeciesEditor().render(true);
-  })
+    `);
+    html.height('auto');
 });
 
 /* -------------------------------------------- */
@@ -644,6 +662,23 @@ Hooks.once("ready", async function() {
 
   PTUCombat.Initialize();
 });
+
+/* -------------------------------------------- */
+/*  Settings Initialization                       */
+/* -------------------------------------------- */
+
+Hooks.on("renderSettings", (app, html) => {
+  html.find('#settings-game').after($(`<h2>PTU System Settings</h2><div id="ptu-options"></div>`));
+
+  game.ptu.settings.Initialize(html);
+
+  $('#ptu-options').append($(
+    `<button data-action="ptu-custom-species-editor">
+        <i class="fas fa-book-open"></i>
+        Edit Custom Species
+    </button>`));
+    html.find('button[data-action="ptu-custom-species-editor"').on("click", _ => new game.ptu.PTUCustomSpeciesEditor().render(true));
+})
 
 /* -------------------------------------------- */
 /*  Canvas Initialization                       */
