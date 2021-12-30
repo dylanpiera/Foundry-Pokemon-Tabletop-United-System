d7e968dd8b73847a01ecdd8b1cf81ec403d2dcd9
GSV A Nest of Kindled Embers <69528253+GSV-a-Nest-of-Kindled-Embers@users.noreply.github.com>
2021-12-29 20:58:20 -0500

    Fixed a few issues related to getting token UUIDs caused by a data structure change in v9
    - Fixed a few issues related to getting token UUIDs caused by a data structure change in v9
    - Changed version compatibility string to match Foundry guidance ("9" instead of "0.9.0")

--- module/combat/combat.js
+++ module/combat/combat.js
@@ -174,8 +174,8 @@ export default class PTUCombat {
         await this._saveData();
       }
     } else {
-      if (!this.data.participants.includes(combatant.token.uuid)) {
-        this.data.participants.push(combatant.token.uuid);
+      if (!this.data.participants.includes(combatant.token.document.uuid)) {
+        this.data.participants.push(combatant.token.document.uuid);
         await this._saveData();
       }
     }
