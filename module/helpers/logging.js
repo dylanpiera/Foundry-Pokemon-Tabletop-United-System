2b525a3f7a8a28c72086008317c0cf1200dd828f
Ashe <ashe@soaringnetwork.com>
2022-07-21 13:52:23 +0200

    Start of new `game.ptu` standard.

---
+++ module/helpers/logging.js
@@ -0,0 +1,20 @@
+export default logger = {
+    log: function(...args) {
+        console.log(`PTU | Log: `, ...args)
+    },
+    debug: function(...args) {
+        const r = randomID();
+        console.debug(`PTU | Debug (${r}): `, ...args)
+        console.trace(`PTU | Debug (${r}): `)
+    },
+    warn: function(...args) {
+        const r = randomID();
+        console.warn(`PTU | Warning (${r}): `, ...args)
+        console.trace(`PTU | Warning (${r}): `)
+    },
+    error: function(...args) {
+        const r = randomID();
+        console.error(`PTU | Error (${r}): `, ...args)
+        console.trace(`PTU | Error (${r}): `)
+    },
+}
--- module/ptu.js
+++ module/ptu.js
@@ -23,7 +23,7 @@ import { PTUCustomMonEditor } from './forms/custom-mon-editor-form.js'
 import { PTUCharacterNotesForm } from './forms/character-notes-form.js'
 import { RollWithDb } from './utils/roll-calculator.js'
 import { InitCustomSpecies, UpdateCustomSpecies } from './custom-species.js'
-import { InitCustomTypings, UpdateCustomTypings } from './custom-typings.js'
+import { InitCustomTypings as initCustomTypings, UpdateCustomTypings } from './custom-typings.js'
 import { ChangeLog } from './forms/changelog-form.js'
 import { applyDamageToTargets, undoDamageToTargets, newApplyDamageToTargets, handleApplicatorItem, TakeAction } from './combat/damage-calc-tools.js'
 import CustomSpeciesFolder from './entities/custom-species-folder.js'
@@ -47,8 +47,7 @@ import TMsData from './data/tm-data.js'
 import PTUActiveEffectConfig from './forms/active-effect-config.js';
 import getTrainingChanges from './data/training-data.js';
 import { PTUSettings, PTUSettingCategories } from './forms/settings.js';
-import { LoadSystemSettings, SetAccessabilityFont } from './settings.js'
-import PreloadHandlebarsTemplates from './templates.js'
+import { LoadSystemSettings as loadSystemSettings, SetAccessabilityFont } from './settings.js'
 import Store from "./api/front-end/lib/store.js";
 import Component from "./api/front-end/lib/component.js";
 import { PTUSidebar } from "./sidebar/sidebar-form.js";
@@ -56,20 +55,109 @@ import './utils/item-piles-compatibility-handler.js';
 import './utils/drag-ruler-compatibility-handler.js';
 import { ThrowPokeball } from './combat/effects/pokeball_effects.js';
 import { LANG } from './utils/language-helper.js';
+import logging from "./helpers/logging.js";
+import {registerHandlebars, preloadHandlebarsTemplates} from "./helpers/handlebars.js";
 
-export let debug = (...args) => { if (game.settings.get("ptu", "showDebugInfo") ?? false) console.log("DEBUG: FVTT PTU | ", ...args) };
-export let log = (...args) => console.log("FVTT PTU | ", ...args);
-export let warn = (...args) => console.warn("FVTT PTU | ", ...args);
-export let error = (...args) => console.error("FVTT PTU | ", ...args)
+export let debug = logging.debug;
+export let log = logging.log;
+export let warn = logging.warn;
+export let error = logging.error;
 
 export const LATEST_VERSION = "3.0.0.1";
+
+export const ptu = {
+  utils: {
+    api: { 
+      gm: undefined,
+      ui: {
+        Store,
+        Component
+      }
+    },
+    cache: {
+      
+    },
+    dice: {
+      dbRoll: RollWithDb
+    },
+    generator: {
+      ActorGenerator,
+      CreateMonParser,
+      GetRandomNature,
+      GiveRandomAbilities,
+      GiveLatestMoves,
+      ApplyEvolution,
+      GiveCapabilities,
+      StatDistributions: {
+        DistributeStatsWeighted,
+        DistributeStatsRandomly,
+        DistributeByBaseStats,
+        BaseStatsWithNature,
+        ApplyLevelUpPoints
+      },
+      GetSpeciesArt
+    },
+    logging,
+    species: {
+      get: GetSpeciesData,
+      playCry: PlayPokemonCry,
+
+    },
+  },
+  config: {
+    ActiveEffect: {
+      sheetClass: PTUActiveEffectConfig
+    },
+    Actor: {
+      documentClass: PTUActor,
+      sheetClasses: {
+        character: PTUGen8CharacterSheet,
+        pokemon: PTUGen8PokemonSheet
+      }
+    },
+    Combat: {
+      documentClass: PTUCombatOverrides,
+      defeatedStatusId: "effect.other.fainted"
+    },
+    Item: {
+      documentClass: PTUItem,
+      sheetClasses: {
+        item: PTUItemSheet,
+        edge: PTUEdgeSheet,
+        feat: PTUFeatSheet
+      }
+    },
+    Settings: {
+      documentClass: PTUSettings,
+      categories: PTUSettingCategories
+    },
+    
+    ui: {
+      Combat: {
+        documentClass: PTUCombatTrackerOverrides
+      }
+    }
+  },
+  data: {
+    levelProgression,
+    pokemonData,
+    customSpeciesData: [],
+    natureData,
+    DbData,
+    TMsData,
+    TypeEffectiveness,
+  }
+}
 
 /* -------------------------------------------- */
 /*  Foundry VTT Initialization                  */
 /* -------------------------------------------- */
 
 Hooks.once('init', function () {
-  console.groupCollapsed("PTU Init");
+  console.groupCollapsed("PTU Init"); 
+  console.time("PTU Init")
+
+  game.ptu_new = ptu;
 
   // Create a namespace within the game global
   game.ptu = {
@@ -137,29 +225,31 @@ Hooks.once('init', function () {
     }
   };
 
+  ptu.utils.api.gm = game.ptu.api;
+
   /**
    * Set an initiative formula for the system
    * @type {String}
    */
-  CONFIG.Combat.initiative = {
+  CONFIG.Combat.initiative = ptu.config.Combat.initiative = {
     formula: "@initiative.value + (1d20 * 0.01)",
     decimals: 2
   };
   // Initialize custom initative hooks
-  CONFIG.Combat.documentClass=PTUCombatOverrides;
+  CONFIG.Combat.documentClass = ptu.config.Combat.documentClass;
 
   // Define custom combat tracker
-  CONFIG.ui.combat = PTUCombatTrackerOverrides;
-
+  CONFIG.ui.combat = ptu.config.ui.Combat.documentClass;
+  
   // Define custom Entity classes
-  CONFIG.Actor.documentClass = PTUActor;
-  CONFIG.Item.documentClass = PTUItem;
+  CONFIG.Actor.documentClass = ptu.config.Actor.documentClass;
+  CONFIG.Item.documentClass = ptu.config.Item.documentClass;
 
   // Define custom Active Effect class
-  CONFIG.ActiveEffect.sheetClass = PTUActiveEffectConfig;
+  CONFIG.ActiveEffect.sheetClass = ptu.config.ActiveEffect.sheetClass;
 
   // Custom Combat Settings
-  CONFIG.Combat.defeatedStatusId = "effect.other.fainted";
+  CONFIG.Combat.defeatedStatusId = ptu.config.Combat.defeatedStatusId;
 
   // Register sheet application classes
   registerSheets();
@@ -168,7 +258,7 @@ Hooks.once('init', function () {
   registerHandlebars();
 
   // Load System Settings
-  LoadSystemSettings();
+  loadSystemSettings();
 
   if (game.settings.get("ptu", "insurgenceData")) {
     Array.prototype.push.apply(game.ptu["pokemonData"], insurgenceData);
@@ -180,11 +270,12 @@ Hooks.once('init', function () {
     Array.prototype.push.apply(game.ptu["pokemonData"], uraniumData);
   }
 
-  InitCustomTypings();
+  initCustomTypings();
 
   // Preload Handlebars Templates
-  PreloadHandlebarsTemplates();
+  preloadHandlebarsTemplates();
 
+  console.timeEnd("PTU Init")
   console.groupEnd();
 });
 
@@ -192,228 +283,12 @@ Hooks.once('init', function () {
 function registerSheets() {
   // Register sheet application classes
   Actors.unregisterSheet("core", ActorSheet);
-  Actors.registerSheet("ptu", PTUGen8CharacterSheet, { types: ["character"], makeDefault: true });
-  Actors.registerSheet("ptu", PTUGen8PokemonSheet, { types: ["pokemon"], makeDefault: true });
+  Actors.registerSheet("ptu", ptu.config.Actor.sheetClasses.character, { types: ["character"], makeDefault: true });
+  Actors.registerSheet("ptu", ptu.config.Actor.sheetClasses.pokemon, { types: ["pokemon"], makeDefault: true });
   Items.unregisterSheet("core", ItemSheet);
-  Items.registerSheet("ptu", PTUItemSheet, { types: ["item", "ability", "move", "capability", "pokeedge", "dexentry"], makeDefault: true });
-  Items.registerSheet("ptu", PTUEdgeSheet, { types: ["edge"], makeDefault: true });
-  Items.registerSheet("ptu", PTUFeatSheet, { types: ["feat"], makeDefault: true });
-}
-
-function registerHandlebars() {
-  Handlebars.registerHelper("concat", function () {
-    var outStr = '';
-    for (var arg in arguments) {
-      if (typeof arguments[arg] != 'object') {
-        outStr += arguments[arg];
-      }
-    }
-    return outStr;
-  });
-
-  Handlebars.registerHelper("toLowerCase", function (str) {
-    return str.toLowerCase ? str.toLowerCase() : str;
-  });
-
-  Handlebars.registerHelper("isdefined", function (value) {
-    return value !== undefined;
-  });
-
-  Handlebars.registerHelper("key", function (obj) {
-    return Object.keys(obj)[0];
-  });
-
-  Handlebars.registerHelper("is", function (a, b) { return a == b });
-  Handlebars.registerHelper("bigger", function (a, b) { return a > b });
-  Handlebars.registerHelper("biggerOrEqual", function (a, b) { return a >= b });
-  Handlebars.registerHelper("and", function (a, b) { return a && b });
-  Handlebars.registerHelper("or", function (a, b) { return a || b });
-  Handlebars.registerHelper("not", function (a, b) { return a != b });
-  Handlebars.registerHelper("itemDescription", function (name) {
-    if (!name) return "";
-    if (name || 0 !== name.length) {
-      let item = game.ptu.items.find(i => i.name.toLowerCase().includes(name.toLowerCase()));
-      if (item) return item.system.effect;
-    }
-    return "";
-  });
-  Handlebars.registerHelper("getGameSetting", function (key) { return game.settings.get("ptu", key) });
-  Handlebars.registerHelper("calcDb", function (move) {
-    return (move.damageBase.toString().match(/^[0-9]+$/) != null) ? move.stab ? parseInt(move.damageBase) + 2 : move.damageBase : move.damageBase;
-  });
-  Handlebars.registerHelper("calcDbCalc", _calcMoveDb);
-  Handlebars.registerHelper("calcAc", function (move) {
-    return -parseInt(move.ac) + parseInt(move.acBonus);
-  });
-  Handlebars.registerHelper("calcMoveDb", function (actorData, move, bool = false) {
-    return _calcMoveDb(PrepareMoveData(actorData, move), bool);
-  });
-  Handlebars.registerHelper("calcCritRange", function (actorData) {
-    return actorData.modifiers.critRange?.total ? actorData.modifiers.critRange?.total : 0;
-  });
-  Handlebars.registerHelper("calcCritRangeMove", function (move) {
-    return move.owner ? move.owner.critRange : 0;
-  });
-  Handlebars.registerHelper("getProperty", getProperty);
-  Handlebars.registerHelper("aeTypes", function (id) {
-    const types = Object.entries(CONST.ACTIVE_EFFECT_MODES).reduce((obj, e) => {
-      obj[e[1]] = game.i18n.localize("EFFECT.MODE_" + e[0]);
-      return obj;
-    }, {});
-    return id ? types[id] : types;
-  })
-
-  Handlebars.registerHelper("calcFrequencyIconPath", function (frequency, currentUseCount) {
-    const basePath = "systems/ptu/images/icons/";
-    const useCount = Number(currentUseCount);
-    switch (frequency) {
-      case "At-Will":
-      case "":
-        return basePath + "AtWill" + ".png";
-      case "EOT":
-        return basePath + (useCount == 0 ? "EOT_1" : "EOT_0") + ".png";
-      case "Scene":
-        return basePath + (useCount >= 1 ? "Scene1_0" : "Scene1_1") + ".png";
-      case "Scene x2":
-        return basePath + (useCount >= 2 ? "Scene2_0" : useCount == 1 ? "Scene2_1" : "Scene2_2") + ".png";
-      case "Scene x3":
-        return basePath + (useCount >= 3 ? "Scene3_0" : useCount == 2 ? "Scene3_1" : useCount == 1 ? "Scene3_2" : "Scene3_3") + ".png";
-      case "Daily":
-        return basePath + (useCount >= 1 ? "daily1_0" : "daily1_1") + ".png";
-      case "Daily x2":
-        return basePath + (useCount >= 2 ? "daily2_0" : useCount == 1 ? "daily2_1" : "daily2_2") + ".png";
-      case "Daily x3":
-        return basePath + (useCount >= 3 ? "daily3_0" : useCount == 2 ? "daily3_1" : useCount == 1 ? "daily3_2" : "daily3_3") + ".png";
-    }
-  })
-
-  function keyToNatureStat(key) {
-    switch (key) {
-      case "hp": return "HP";
-      case "atk": return "Attack";
-      case "def": return "Defense";
-      case "spatk": return "Special Attack";
-      case "spdef": return "Special Defense";
-      case "spd": return "Speed";
-    }
-  }
-
-  Handlebars.registerHelper("natureCheck", function (nature, stat) {
-    let statUp = game.ptu.natureData[nature][0] == keyToNatureStat(stat);
-    let statDown = game.ptu.natureData[nature][1] == keyToNatureStat(stat)
-
-    return statUp && !statDown ? "nature-up" : statDown && !statUp ? "nature-down" : "";
-  });
-
-  Handlebars.registerHelper("minMaxDiceCheck", function (roll, faces) {
-    return roll == 1 ? "min" : roll == faces ? "max" : "";
-  });
-
-  Handlebars.registerHelper("hideAcOrDb", function (text) {
-    return text == "" || text == "--";
-  });
-
-  Handlebars.registerHelper("loadTypeImages", function (types, includeSlash = true) {
-    // TypeEffectiveness here is imported from source and only contains the default types
-    // in contract, game.ptu.TypeEffectiveness contains custom types as well
-    const isTypeDefaultType = (typeName) => Object.keys(TypeEffectiveness).includes(typeName)
-    let customDir = game.settings.get("ptu", "typeEffectivenessCustomImageDirectory");
-    if (customDir.slice(-1) !== "/") customDir += "/"
-    if (customDir.charAt(0) !== "/") customDir = "/" + customDir
-    if (!types) return;
-    return types.reduce((html, type, index, array) => {
-      if (type == "null") type = "Untyped";
-      if (isTypeDefaultType(type)) return html += `<img class="mr-1 ml-1" src="/systems/ptu/css/images/types/${type}IC.webp">` + (includeSlash ? (index != (array.length - 1) ? "<span>/</span>" : "") : "");
-      else return html += `<img class="mr-1 ml-1" src="${customDir}${type}IC.webp">` + (includeSlash ? (index != (array.length - 1) ? "<span>/</span>" : "") : "");
-    }, "")
-  });
-
-  Handlebars.registerHelper("loadTypeImage", function (type) {
-    // TypeEffectiveness here is imported from source and only contains the default types
-    // in contract, game.ptu.TypeEffectiveness contains custom types as well
-    const isTypeDefaultType = (typeName) => Object.keys(TypeEffectiveness).includes(typeName)
-    let customDir = game.settings.get("ptu", "typeEffectivenessCustomImageDirectory");
-    if (customDir.slice(-1) !== "/") customDir += "/"
-    if (customDir.charAt(0) !== "/") customDir = "/" + customDir
-    if (isTypeDefaultType(type)) return `<img src="/systems/ptu/css/images/types/${type}IC.webp">`;
-    else return `<img src="${customDir}${type}IC.webp">`
-  });
-
-  Handlebars.registerHelper("isGm", function () {
-    return game.user.isGM;
-  })
-
-  Handlebars.registerHelper("ld", function (key, value) {
-    return { hash: { [key]: value } };
-  })
-
-  Handlebars.registerHelper("toReadableEffectMode", function (effectId) {
-    return Object.entries(CONST.ACTIVE_EFFECT_MODES).reduce((obj, e) => {
-      obj[e[1]] = game.i18n.localize("EFFECT.MODE_" + e[0]);
-      return obj;
-    }, {})[effectId]
-  })
-
-  Handlebars.registerHelper('getEffectivenessColor', function (effectiveness) {
-    const value = Number(effectiveness);
-    if (isNaN(value)) return "regular";
-
-    if(value === -1) return "none";
-    if (value < 1) {
-      if (value == 0) {
-        return "immune";
-      }
-      if(value <= 0.25) {
-        return "doubly_resisted";
-      }
-      return "resisted";
-    }
-    if (value > 1) {
-      if(value >= 2) {
-        return "doubly_effective";
-      }
-      return "effective";
-    }
-    return "regular";
-  })
-
-  Handlebars.registerHelper('contains', function (needle, haystack) {
-    needle = Handlebars.escapeExpression(needle);
-    haystack = Handlebars.escapeExpression(haystack);
-    return (haystack.indexOf(needle) > -1) ? true : false;
-  });
-
-  Handlebars.registerHelper('ifContains', function (needle, haystack, options) {
-    needle = Handlebars.escapeExpression(needle);
-    haystack = Handlebars.escapeExpression(haystack);
-    return (haystack.indexOf(needle) > -1) ? options.fn(this) : options.inverse(this);
-  });
-
-  Handlebars.registerHelper("inc", function (num) { return Number(num) + 1 })
-
-  Handlebars.registerHelper("tmName", function (tmNum) { return game.ptu.TMsData.get(tmNum) });
-
-  /** If furnace ain't installed... */
-  if (!Object.keys(Handlebars.helpers).includes("divide")) {
-
-    Handlebars.registerHelper("divide", (value1, value2) => Number(value1) / Number(value2));
-    Handlebars.registerHelper("multiply", (value1, value2) => Number(value1) * Number(value2));
-    Handlebars.registerHelper("floor", (value) => Math.floor(Number(value)));
-    Handlebars.registerHelper("capitalizeFirst", (e) => { return "string" != typeof e ? e : e.charAt(0).toUpperCase() + e.slice(1) });
-  }
-
-  function _calcMoveDb(move, bool = false) {
-    if (move.category === "Status") return;
-    let bonus = (move.owner ? move.category === "Physical" ? (move.owner.stats.atk.total + (move.owner.damageBonus?.physical?.total ?? 0)) : (move.owner.stats.spatk.total + (move.owner.damageBonus?.special?.total ?? 0)) : 0) + (move.damageBonus ?? 0);
-    if (move.damageBase.toString().match(/^[0-9]+$/) != null) {
-      let db = game.ptu.DbData[move.stab ? parseInt(move.damageBase) + 2 : move.damageBase];
-      if (db) return db + (bool ? " + " : "#") + bonus;
-      return -1;
-    }
-    let db = game.ptu.DbData[move.damageBase];
-    if (db) return db;
-    return -1;
-  }
+  Items.registerSheet("ptu", ptu.config.Item.sheetClasses.item, { types: ["item", "ability", "move", "capability", "pokeedge", "dexentry"], makeDefault: true });
+  Items.registerSheet("ptu", ptu.config.Item.sheetClasses.edge, { types: ["edge"], makeDefault: true });
+  Items.registerSheet("ptu", ptu.config.Item.sheetClasses.feat, { types: ["feat"], makeDefault: true });
 }
 
 export function PrepareMoveData(actorData, move) {
@@ -520,7 +395,7 @@ Hooks.once("ready", async function () {
   }
 
   await InitCustomSpecies();
-  await InitCustomTypings();
+  await initCustomTypings();
 
   SetAccessabilityFont(game.settings.get("ptu", "accessability"));
 
