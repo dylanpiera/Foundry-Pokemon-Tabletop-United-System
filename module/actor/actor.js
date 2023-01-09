a7a1144086ca042b9606058d2641a9672b5424b1
Ellamental2 <ellammalle_1994@hotmail.com>
2023-01-09 20:58:52 +0000

    cleaned code

--- module/actor/actor.js
+++ module/actor/actor.js
@@ -647,78 +647,65 @@ export class PTUActor extends Actor {
       // TODO: Apply Attack (+ effects) 
     }, game.settings.get("ptu", "dramaticTiming") == true ? move_animation_delay_ms : 0);
     
-    // If move used was transform and it was used by a ditto:
+    // If move used was transform
     if (moveData.name.toLowerCase() =="transform") {
       this.transform();
     }
   }
 
   async transform() {
-    let selected = canvas.tokens.controlled;
-    if (selected.length == 0 || selected.length > 1) {
+
+    if (canvas.tokens.controlled.length == 0 || canvas.tokens.controlled.length > 1) {
         ui.notifications.warn("Please select a single token.");
         return;
     }
 
-    let selectedActor = selected[0].actor;
-    let tok = selectedActor.getActiveTokens()[0];
+    const selectedActor = canvas.tokens.controlled[0].actor;
+    const transformingToken = selectedActor.getActiveTokens()[0];
 
-    let alreadyTransformed = false;
-    
-
-    //if tok already has an effect with the label "Transform" then alreadyTransformed = true
-    selectedActor.effects.forEach(effect => {
-      if (effect.data.label == "Transform") {
-        alreadyTransformed = true;
-      }
-    })
+    const alreadyTransformed = selectedActor.effects.find(effect => effect.data.label == "Transform");
 
     if (!alreadyTransformed){
 
       //change from origonal pokemon to target
-      let targets = Array.from(game.user.targets);
-      if (targets.length == 0 || targets.length > 1) {
+      const targets = [...game.user.targets];
+      if (targets.length != 1) {
           ui.notifications.warn("Unable to transform - Please target a single target.");
           return;
       }
 
+      //this will be added to the actors moves to enable them to transform back
       const transformMove = selectedActor.items.find(item => item.name.toLowerCase() == "transform");
 
-      const dittoItems = {
-        ['flags.ptu.dittoItems']: selectedActor.items.filter(item => item.type === "move" || item.type === "ability" || item.type === "capability" )
-      };
-
-      const imageFlag = {
-        ['flags.ptu.dittoImage']: selectedActor.img
+      const oldData = {
+        ['flags.ptu.initialItems']: selectedActor.items.filter(item => item.type === "move" || item.type === "ability" || item.type === "capability" ),
+        ['flags.ptu.initialImage']: selectedActor.img,
+        ['flags.ptu.initialSize']: selectedActor.prototypeToken.height
       }
 
-      const sizeFlag = {
-        ['flags.ptu.dittoSize']: selectedActor.prototypeToken.height
-      }
+      await selectedActor.update(oldData);
 
-      selectedActor.update(dittoItems);
-      selectedActor.update(imageFlag);
-      selectedActor.update(sizeFlag);
+      const targetActor = targets[0].actor;
 
-      let targetActor = targets[0].actor;
-
-      let targetImage = targetActor.img;
-
-      let targetSize = targetActor.prototypeToken.height; 
-
-      let targetItems = targetActor.items.filter(item => item.type === "move" || item.type === "ability" || item.type === "capability" ).map(item => item.data);
-      //add transformMove to targetItems so that ditto can turn back
-      targetItems.push(transformMove.data);
+      const targetItems = [...targetActor.items.filter(item => item.type === "move" || item.type === "ability" || item.type === "capability" ).map(item => item.data), transformMove.data];
 
       //update token
-      const updates = {_id: tok.id, height: targetSize, width: targetSize, img: targetImage};
-      tok.document.update(updates);
+      const updates = {
+        _id: transformingToken.id,
+        height: targetActor.prototypeToken.height,
+        width: targetActor.prototypeToken.width,
+        img: targetActor.img
+      };
+
+      await transformingToken.document.update(updates);
       //update capabilities of selectedActor
       //selectedActor.capabilities = targetCapabilites;
       //delete all current moves and abiltites
-      selectedActor.items.filter(item => item.type === "move" || item.type === "ability" || item.type === "capability").forEach(item => item.delete());
+
+      await selectedActor.deleteEmbeddedDocuments("Item", selectedActor.items.filter(item => item.type === "move" || item.type === "ability" || item.type === "capability").map(item => item.id));
+
       //add new moves and abilities
-      selectedActor.createEmbeddedDocuments("Item", duplicate(targetItems))
+      await selectedActor.createEmbeddedDocuments("Item", duplicate(targetItems))
       //add effects to change capabilities
       const TransformEffect = {
           "changes": [
@@ -737,24 +724,30 @@ export class PTUActor extends Actor {
       await selectedActor.createEmbeddedDocuments("ActiveEffect", [TransformEffect]);
 
     } else {
-        //change back to ditto
-        const dittoItems = selectedActor.getFlag('ptu', 'dittoItems');
-        const dittoImage = selectedActor.getFlag('ptu', 'dittoImage');
-        const sizeDitto = selectedActor.getFlag('ptu', 'dittoSize');
+        //change back to initial mon
+        const initialItems = selectedActor.getFlag('ptu', 'initialItems');
+        const initialImage = selectedActor.getFlag('ptu', 'initialImage');
+        const initialSize = selectedActor.getFlag('ptu', 'initialSize');
 
-        tok.document.unsetFlag('ptu', 'dittoItems');
-        tok.document.unsetFlag('ptu', 'dittoImage');
-        tok.document.unsetFlag('ptu', 'dittoSize');
+        //remove flags
+        await selectedActor.update({'flags.ptu.-=initialItems': false,
+          'flags.ptu.-=initialImage': false,
+          'flags.ptu.-=initialSize': false
+        });
+
         //update token
-        const updates = {_id: tok.id, height: sizeDitto, width: sizeDitto, img: dittoImage};
-        tok.document.update(updates);
+        await transformingToken.document.update({_id: transformingToken.id,
+          height: initialSize,
+          width: initialSize,
+          img: initialImage
+        });
         //delete all current moves and abiltites
-        selectedActor.items.filter(item => item.type === "move" || item.type === "ability" || item.type === "capability").forEach(item => item.delete());
-        //add new moves and abilities
-        selectedActor.createEmbeddedDocuments("Item", duplicate(dittoItems))
+        await selectedActor.deleteEmbeddedDocuments("Item", selectedActor.items.filter(item => item.type === "move" || item.type === "ability" || item.type === "capability").map(item => item.id));
+
+        //add old moves and abilities
+        await selectedActor.createEmbeddedDocuments("Item", duplicate(initialItems))
         //remove effects to change capabilities
-        selectedActor.effects.filter(e => e.label.toLowerCase() === "transform").forEach(effect => effect.delete())
-        
+        await selectedActor.deleteEmbeddedDocuments("ActiveEffect", selectedActor.effects.filter(e => e.label.toLowerCase() === "transform").map(e => e.id));
     }
   }
 
