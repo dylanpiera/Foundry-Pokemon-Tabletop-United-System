4feaa534ed36438bbca9d9f9520867be933c73ef
Ashe <ashe@soaringnetwork.com>
2023-03-16 16:16:07 +0100

    Level Up Form with Stats

--- module/actor/actor.js
+++ module/actor/actor.js
@@ -391,7 +391,7 @@ export class PTUActor extends Actor {
 
     for (let [key, mod] of Object.entries(data.modifiers)) {
       if (key == "hardened" || key == "flinch_count" || key == 'immuneToEffectDamage') continue;
-      if (key == "damageBonus" || key == "damageReduction" || key == "evasion") {
+      if (key == "damageBonus" || key == "damageReduction" || key == "evasion" || key == "baseStats") {
         for (let [subkey, value] of Object.entries(mod)) {
           data.modifiers[key][subkey]["total"] = (value["value"] ?? 0) + (value["mod"] ?? 0);
         }
@@ -414,6 +414,20 @@ export class PTUActor extends Actor {
 
     data.levelUpPoints = data.level.current + data.modifiers.statPoints.total + 9;
     data.stats = CalculatePoisonedCondition(duplicate(data.stats), actorData.flags?.ptu);
+    
+    data.stats.hp.base = 10
+    data.stats.hp.value = data.modifiers.baseStats.hp.total + data.stats.hp.base;
+    data.stats.atk.base = 5
+    data.stats.atk.value = data.modifiers.baseStats.atk.total + data.stats.atk.base;
+    data.stats.def.base = 5
+    data.stats.def.value = data.modifiers.baseStats.def.total + data.stats.def.base;
+    data.stats.spatk.base = 5
+    data.stats.spatk.value = data.modifiers.baseStats.spatk.total + data.stats.spatk.base;
+    data.stats.spdef.base = 5
+    data.stats.spdef.value = data.modifiers.baseStats.spdef.total + data.stats.spdef.base;
+    data.stats.spd.base = 5
+    data.stats.spd.value = data.modifiers.baseStats.spd.total + data.stats.spd.base;
+
     var result = game.settings.get("ptu", "playtestStats") ?
       CalculatePTStatTotal(data.levelUpPoints, data.level.current, data.stats, { twistedPower: actorData.items.find(x => x.name.toLowerCase().replace("[playtest]") == "twisted power") != null }, data.nature?.value, true) :
       CalculateStatTotal(data.levelUpPoints, data.stats, { twistedPower: actorData.items.find(x => x.name.toLowerCase().replace("[playtest]") == "twisted power") != null });
@@ -465,7 +479,7 @@ export class PTUActor extends Actor {
     // Prepare data with Mods.
     for (let [key, mod] of Object.entries(data.modifiers)) {
       if (key == "hardened" || key == "flinch_count" || key == 'immuneToEffectDamage') continue;
-      if (key == "damageBonus" || key == "damageReduction" || key == "evasion") {
+      if (key == "damageBonus" || key == "damageReduction" || key == "evasion" || key == "baseStats") {
         for (let [subkey, value] of Object.entries(mod)) {
           data.modifiers[key][subkey]["total"] = (value["value"] ?? 0) + (value["mod"] ?? 0);
         }
@@ -487,7 +501,7 @@ export class PTUActor extends Actor {
     // Stats
     data.stats = CalculatePoisonedCondition(duplicate(data.stats), actorData.flags?.ptu);
 
-    data.stats = CalcBaseStats(data.stats, speciesData, data.nature.value);
+    data.stats = CalcBaseStats(data.stats, speciesData, data.nature.value, data.modifiers.baseStats);
 
     var result = game.settings.get("ptu", "playtestStats") ?
     CalculatePTStatTotal(data.levelUpPoints, data.level.current, data.stats, { twistedPower: actorData.items.find(x => x.name.toLowerCase().replace("[playtest]") == "twisted power") != null }, data.nature.value, false) :
