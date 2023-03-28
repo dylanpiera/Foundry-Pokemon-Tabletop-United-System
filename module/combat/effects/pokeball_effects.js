8b5a8b1cbf29ed3af9487ca5232035afaec22708
Ellamental2 <ellammalle_1994@hotmail.com>
2023-03-28 07:58:27 +0100

    kind of fixed the animation for sending out mons

--- module/combat/effects/pokeball_effects.js
+++ module/combat/effects/pokeball_effects.js
@@ -497,7 +497,7 @@ export async function PlayReleaseOwnedPokemonAnimation(token) {
             if(enable_pokeball_animation)
             {
                 // await target_token.document.update({ "alpha": (0) });
-                await game.ptu.utils.api.gm.updateToken(target_token, {alpha: 0})
+                await game.ptu.utils.api.gm.tokensUpdate(target_token, {alpha: 0})
             }
 
             await AudioHelper.play({src: pokeball_sound_paths["miss"], volume: 0.5, autoplay: true, loop: false}, true);
@@ -527,7 +527,7 @@ export async function PlayReleaseOwnedPokemonAnimation(token) {
                 // await target_token.TMFXaddUpdateFilters(pokeballShoop_params); 
                 await game.ptu.utils.api.gm.addTokenMagicFilters(target_token, game.canvas.scene, pokeballShoop_params);
                 // await target_token.document.update({ "alpha": (1) });
-                await game.ptu.utils.api.gm.updateToken(target_token, {alpha: 1})
+                await game.ptu.utils.api.gm.tokensUpdate(target_token, {alpha: 1})
             }
 
             await timeout(2000);
