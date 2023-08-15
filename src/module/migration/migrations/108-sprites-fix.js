30f7801ef448948a7b27391a4057da48607fbcd3
Ashe <ashe@soaringnetwork.com>
2023-08-15 19:49:17 +0200

    Sprite Migration only Pokemon

--- src/module/migration/migrations/108-sprites-fix.js
+++ src/module/migration/migrations/108-sprites-fix.js
@@ -7,6 +7,7 @@ export class Migration108SpritesFix extends MigrationBase {
      * @type {MigrationBase['updateActor']}
      */
     async updateActor(actor) {
+        if(actor.type !== "pokemon") return;
         const match = /(\d+.+).(webp|png)/.exec(actor.img);
         if(!match) return;
 
@@ -18,6 +19,7 @@ export class Migration108SpritesFix extends MigrationBase {
     }
 
     async updateToken(token) {
+        if(game.actors.get(token.delta._id).type !== "pokemon") return;
         const match = /(\d+.+).(webp|png)/.exec(token.texture.src);
         if(!match) return;
 
