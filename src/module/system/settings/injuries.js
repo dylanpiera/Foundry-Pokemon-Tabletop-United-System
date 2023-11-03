ef3cf65db7a66315b5ce858da62af1e3f1b087d0
Tinkastrong <tinkastrong@bokz.eu>
2023-11-03 12:37:44 +0100

    Removed comments and development logging

--- src/module/system/settings/injuries.js
+++ src/module/system/settings/injuries.js
@@ -1,33 +1,5 @@
 import { PTUSettingsMenu } from "./base.js";
 
-/*
-
-    //TODO wording
-    game.settings.register("ptu", "injuries.massiveDamageThresholdRollFormula", {
-        scope: "world",
-        config: true,
-        default: "1/2",
-        type: String,
-    });
-
-    //TODO wording
-    game.settings.register("ptu", "injuries.hpInjuryGateIntervalRollFormula", {
-        scope: "world",
-        config: true,
-        default: "1/2",
-        type: String,
-    });
-
-    <div class="form-group">
-                <label for="massiveDamageThresholdRollFormula">PTU.Settings.Injuries.MassiveDamageThreshold.Name</label>
-                <div class="form-fields">
-                        <input type="text" name="massiveDamageThresholdRollFormula" value="1/2">
-                                    </div>
-                <p class="notes">PTU.Settings.Injuries.MassiveDamageThreshold.Hint</p>
-            </div>
-
- */
-
 const InjuriesSettingsConfig = {
     "massiveDamageThresholdRollFormula":{
         name: "PTU.Settings.Injuries.MassiveDamageThreshold.Name",
@@ -45,39 +17,6 @@ const InjuriesSettingsConfig = {
         default: "1/2",
         type: String,
     }
-    // "useTutorPoints": {
-    //     name: "PTU.Settings.Variant.UseTutorPoints.Name",
-    //     hint: "PTU.Settings.Variant.UseTutorPoints.Hint",
-    //     type: Boolean,
-    //     default: true
-    // },
-    // "useDexExp": {
-    //     name: "PTU.Settings.Variant.UseDexExp.Name",
-    //     hint: "PTU.Settings.Variant.UseDexExp.Hint",
-    //     type: Boolean,
-    //     default: false,
-    //     requiresReload: true
-    // },
-    // "trainerRevamp": {
-    //     name: "PTU.Settings.Variant.TrainerRevamp.Name",
-    //     hint: "PTU.Settings.Variant.TrainerRevamp.Hint",
-    //     type: Boolean,
-    //     default: false
-    // },
-    // "spiritPlaytest": {
-    //     name: "PTU.Settings.Variant.SpiritPlaytest.Name",
-    //     hint: "PTU.Settings.Variant.SpiritPlaytest.Hint",
-    //     type: Boolean,
-    //     default: false,
-    //     requiresReload: true
-    // },
-    // "improvedStatsRework": {
-    //     name: "PTU.Settings.Variant.ImprovedStatsRework.Name",
-    //     hint: "PTU.Settings.Variant.ImprovedStatsRework.Hint",
-    //     type: Boolean,
-    //     default: true,
-    //     requiresReload: true
-    // }
 }
 
 export class InjuriesSettings extends PTUSettingsMenu {
@@ -99,17 +38,6 @@ export class InjuriesSettings extends PTUSettingsMenu {
 
     activateListeners($html) {
         super.activateListeners($html);
-        console.log("Tinka log 485")
-        // $html.find("input[name='massiveDamageThresholdRollFormula']").on("input", event => {
-        //     const val = event.target.value
-        //     if (!this._isTextValidRollFormula(val)) event.target.style.backgroundColor = "#ff9999";
-        //     else event.target.style.backgroundColor = "#ffffff00"
-        // });
-        // $html.find("input[name='hpInjuryGateIntervalRollFormula']").on("input", event => {
-        //     const val = event.target.value
-        //     if (!this._isTextValidRollFormula(val)) event.target.style.backgroundColor = "#ff9999";
-        //     else event.target.style.backgroundColor = "#ffffff00"
-        // });
         $html.find("button[type='submit']").on("click", event => {
             let valid = true
             for(const fieldName of ["massiveDamageThresholdRollFormula","hpInjuryGateIntervalRollFormula"]){
