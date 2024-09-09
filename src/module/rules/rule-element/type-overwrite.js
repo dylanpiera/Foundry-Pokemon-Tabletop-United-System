7511b457951637f62ed5e49baf5255c99e940ebe
Ashe <ashe@soaringnetwork.com>
2024-09-09 11:01:58 +0200

    Various fixes
    Resolves #789
    Resolves #788

--- src/module/rules/rule-element/type-overwrite.js
+++ src/module/rules/rule-element/type-overwrite.js
@@ -1,34 +1,42 @@
 import { RuleElementPTU } from "./base.js";
 
 export class TypeOverwriteRuleElement extends RuleElementPTU {
-    constructor(data, item, options = {}) {
-        const { value, overwrite } = data;
-        super(data, item, options);
+  constructor(data, item, options = {}) {
+    const { value, overwrite } = data;
+    super(data, item, options);
 
-        this.value = value;
-        this.overwrite = overwrite;
+    this.value = value;
+    this.overwrite = overwrite;
+  }
+
+  /** @override */
+  afterPrepareData() {
+    const type = this.resolveInjectedProperties(this.value);
+    const isArray = Array.isArray(type);
+    if (isArray && type.some(t => typeof t !== "string")) return this.failValidation("Invalid value field");
+    if (!isArray && typeof type !== "string") return this.failValidation("Invalid value field");
+
+    if (!this.test()) return;
+
+    const typeSet = isArray ? new Set(type.flatMap(v => v.split(','))) : new Set([type].flatMap(v => v.split(',')));
+    const realTypes = typeSet.map(t => t.trim()).filter(t => !!t && CONFIG.PTU.data.typeEffectiveness[t]);
+
+    if (this.overwrite) {
+      for(const type of this.actor.types) {
+        delete this.actor.rollOptions.all[`self:types:${type.toLowerCase()}`];
+      }
+      for (const type of realTypes) {
+        this.actor.rollOptions.all[`self:types:${type.toLowerCase()}`] = true;
+      }
+      return this.actor.synthetics.typeOverride.typing = [...realTypes]
     }
 
-    /** @override */
-    afterPrepareData() {
-        const type = this.resolveInjectedProperties(this.value);
-        const isArray = Array.isArray(type);
-        if(isArray && type.some(t => typeof t !== "string")) return this.failValidation("Invalid value field");
-        if(!isArray && typeof type !== "string") return this.failValidation("Invalid value field");
-
-        if (!this.test()) return;
-
-        const typeSet = isArray ? new Set(type.flatMap(v => v.split(','))) : new Set([type].flatMap(v => v.split(',')));
-        const realTypes = typeSet.map(t => t.trim()).filter(t => !!t && CONFIG.PTU.data.typeEffectiveness[t]);
-
-        if(this.overwrite) {
-            return this.actor.synthetics.typeOverride.typing = [...realTypes]
-        }
-
-        if(!this.actor.synthetics.typeOverride.typing) {
-            this.actor.synthetics.typeOverride.typing = [];
-        }
-        this.actor.synthetics.typeOverride.typing.push(...realTypes);
-        
+    if (!this.actor.synthetics.typeOverride.typing) {
+      this.actor.synthetics.typeOverride.typing = [];
     }
+    this.actor.synthetics.typeOverride.typing.push(...realTypes);
+    for(const type of realTypes) {
+      this.actor.rollOptions.all[`self:types:${type.toLowerCase()}`] = true;
+    }
+  }
 }
